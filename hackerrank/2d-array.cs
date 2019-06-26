24773de70a570118a0d40758b46d5821fefb6c0a
Satish Patel <SSJ.SATISH@GMAIL.COM>
2019-06-26 12:21:35 +0530

    hackerrank test

---
+++ hackerrank/2d-array.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        List<List<int>> matrix = new List<List<int>>();
+        for(int i=0;i<6;i++)
+        {
+            string[] elements = Console.ReadLine().Split(' ');
+            matrix.Add(new List<int>());
+            foreach (var item in elements)
+            {
+                matrix[i].Add(int.Parse(item));
+            }
+        }
+        int max = -100500;
+        int temp = 0;
+        for (int i = 1; i <=4; i++)
+        {
+            for (int j = 1; j <=4; j++)
+            {
+                temp = matrix[i - 1][j + 1] +              + matrix[i + 1][j + 1] +
+                           matrix[i - 1][j]     + matrix[i][j] + matrix[i + 1][j] +
+                           matrix[i - 1][j - 1] +              + matrix[i + 1][j - 1];
+            }
+            
+        }
+    }
+}
---
+++ hackerrank/helloworld.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello World");
+    }
+}
